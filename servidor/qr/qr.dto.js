e14b84eb8b1ba7797709b36cb2003b370352098c
Eduardo Peres <edu@gcperes.com>
2024-04-10 23:53:58 -0300

    apagar tudo

--- servidor/qr/qr.dto.js
+++
@@ -1,7 +0,0 @@
-class qrCodeDTO {
-    constructor(hash) {
-        this.hash = hash;
-    }
-}
-
-module.exports = qrCodeDTO;
--- servidor/user/user.dto.js
+++
@@ -1,9 +0,0 @@
-class UserDTO {
-    constructor({username, senha, tipo}) {
-        this.username = username;
-        this.senha = senha;
-        this.tipo = tipo; // garçom ou admin, 0 ou 1
-    }
-}
-
-module.exports = UserDTO;
