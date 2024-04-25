a9404ee146fd163c8247617152623d03af753334
Eduardo Peres <edu@gcperes.com>
2024-04-24 21:36:21 -0300

    adicionar dto do cardápio

---
+++ backend/src/database/cardapio/dto/Cardapio.dto.ts
@@ -0,0 +1,24 @@
+import { IsString, Length, IsBoolean, IsInt, IsDecimal } from 'class-validator';
+
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreatePratoDTO {
+    @IsInt()
+    idPrato: number;
+
+    @Length(1, 64)
+    @IsString()
+    nomePrato: string;
+    
+    @Length(1, 256)
+    @IsString()
+    descricao: string;
+
+    @IsDecimal({decimal_digits: "2"})
+    preco: number;
+
+    @IsInt()
+    idTipo: number;
+}
+
+export class UpdatePratoDTO extends PartialType(CreatePratoDTO) {}
