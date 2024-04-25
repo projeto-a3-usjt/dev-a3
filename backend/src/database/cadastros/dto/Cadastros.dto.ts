f052c4db3bb3086e73f63265413fafcd77d3cbe8
Eduardo Peres <edu@gcperes.com>
2024-04-24 21:36:30 -0300

    adicionar dto dos cadastros

---
+++ backend/src/database/cadastros/dto/Cadastros.dto.ts
@@ -0,0 +1,24 @@
+import { IsUUID, IsString, Length, Matches, IsBoolean } from 'class-validator';
+
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreateCadastrosDTO {
+    @IsUUID("4")
+    idUsuario: string;
+
+    @Length(1, 32)
+    @IsString()
+    username: string;
+
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/)
+    @IsString()
+    senha: string;
+
+    @IsString()
+    nome: string;
+
+    @IsBoolean()
+    tipoConta: boolean;
+}
+
+export class UpdateCadastroDTO extends PartialType(CreateCadastrosDTO) {}
