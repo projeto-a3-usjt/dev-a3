b68c32d385aec738bd08867186c3fb94f34db51c
Eduardo Peres <edu@gcperes.com>
2024-04-28 21:19:20 -0300

    front/garçom: comunicar com o servidor usando fetch

--- frontend/garcom/src/App.js
+++ frontend/garcom/src/App.js
@@ -1,20 +1,39 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useState } from "react";
+import { login } from "./servidor"
 
 export default function App() {
-  return (
+		const [error, setError] = useState('')
+		
+		async function handleLogin(event) {
+				event.preventDefault();
+				let form = event.nativeEvent.target
+				let result = await login(form[0].value, form[1].value)
+				if (result) { // caso não foi redirecionado
+						let erro = await result.json()
+						setError(erro.message)
+				} else {
+						setError("Erro inesperado.")
+				}
+		}
+		
+		return (
 		<div className="container-fluid mx-auto vh-100 d-flex border align-items-center">
 				<div className="card mx-auto w-50 shadow">
 					 <div className="card-header">
 					    Login
 					 </div>
-					 <form className="p-4 row">
+					 <form className="p-4 row" onSubmit={handleLogin}>
 					    <label className="form-label"> Nome de usuário
 					        <input className="form-control" name="username"></input>
 					    </label>
 					    <label className="form-label"> Senha
 					        <input className="form-control" name="senha"></input>
 					    </label>
-					        <button className="w-25 m-3 btn btn-primary">Entrar</button>
+					    <button className="w-25 m-3 btn btn-primary">Entrar</button>
+						  { error && 
+						      <p className="alert alert-danger">{error}</p>
+							}
 			     </form>
 				</div>
     </div>
---
+++ frontend/garcom/src/servidor.js
@@ -0,0 +1,17 @@
+const url = "http://localhost:3001/cadastros/login"
+
+export async function login(username, senha) {
+		try {
+				let credenciais = { username: username, senha: senha }
+				let r = await fetch(url, {
+						method: "POST",
+						body: JSON.stringify(credenciais),
+						headers: { 'Content-Type': 'application/json' }
+				})
+				// esperamos que se dê certo, o servidor redirecione
+				return r
+		} catch (e) {
+				// somente ocorrerá quando o servidor não responder
+				return e
+		}
+}
