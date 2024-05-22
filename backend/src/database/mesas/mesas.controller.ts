3167dffeca64862afd64b9c590782c27b8ed5a56
Eduardo Peres <edu@gcperes.com>
2024-05-22 01:12:26 -0300

    backend: adicionar controller das mesas + módulo

--- backend/src/database/mesas/mesas.controller.ts
+++ backend/src/database/mesas/mesas.controller.ts
@@ -1,6 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Param, Get, Post, Put, Delete, HttpCode, HttpException, HttpStatus } from '@nestjs/common';
+import { CreateMesaDTO, UpdateMesaDTO } from "./dto/Mesas.dto"
+import { MesasService } from "./mesas.service"
+import { CadastrosService } from "../cadastros/cadastros.service"
 
 @Controller('mesas')
 export class MesasController {
-    
+    constructor(private mesasService: MesasService,
+                // não faço a menor ideia se é assim que se faz isso
+                private cadastrosService: CadastrosService) {}
+
+    @Get()
+    async findAll() {
+        return await this.mesasService.findAll();
+    }
+
+    @Post()
+    async create(@Body('id_usuario') idGarcom: string) {
+        let garcom = await this.cadastrosService.findById(idGarcom)
+        if (garcom) {
+            let a = new CreateMesaDTO
+            a = {idGarcom: garcom}
+            return await this.mesasService.create(a)
+        } else {
+            throw new HttpException("ID do garçom inválido", HttpStatus.BAD_REQUEST)
+        }
+    }
 }
--- backend/src/database/mesas/mesas.module.ts
+++ backend/src/database/mesas/mesas.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MesasService } from './mesas.service';
 import { MesasController } from './mesas.controller';
 import { Mesas } from './Mesas.entity';
+import { CadastrosService } from '../cadastros/cadastros.service';
+import { Cadastros } from '../cadastros/Cadastros.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Mesas])],
+    imports: [TypeOrmModule.forFeature([Mesas]),
+              TypeOrmModule.forFeature([Cadastros])],
     controllers: [MesasController],
-    providers: [MesasService],
+    providers: [MesasService, CadastrosService],
 })
 export class MesasModule {}
