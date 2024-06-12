97c452fad10288f88a43afe6290b412d91f87548
Eduardo Peres <edu@gcperes.com>
2024-06-12 00:22:36 -0300

    backend: refatorar dto do qr code

--- backend/src/qrcodes/dto/QrCodes.dto.ts
+++ backend/src/qrcodes/dto/QrCodes.dto.ts
@@ -1,25 +1,23 @@
 import { IsString, Length, IsNotEmpty, IsInt } from "class-validator";
 import { PartialType } from "@nestjs/mapped-types";
+import { Mesas } from "src/mesas/Mesas.entity";
 
 export class CreateQrCodeDTO {
 
     @IsInt()
-    @IsNotEmpty()
-    readonly idQr: number;
+    idQr?: number;
 
     @IsString()
     @Length(6, 6)
-    @IsNotEmpty()
-    readonly hash: string;
+    hash?: string;
 
     @IsString()
     @Length(1, 32)
     @IsNotEmpty()
     readonly nome: string;
 
-    @IsInt()
     @IsNotEmpty()
-    readonly idMesa: number;    
+    readonly idMesa: Mesas;    
 }
 
 export class UpdateQrCodeDTO extends PartialType(CreateQrCodeDTO) {}
