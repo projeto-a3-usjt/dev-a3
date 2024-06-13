b4ba6f221f90700b2e6fd32324bbd422a0fb027c
Eduardo Peres <edu@gcperes.com>
2024-06-13 19:29:23 -0300

    backend: ligar tudo relevante ao módulo qrcodes

--- backend/src/qrcodes/QrCodes.module.ts
+++ backend/src/qrcodes/QrCodes.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { QrCodesService } from './QrCodes.service';
 import { QrcodesController } from './QrCodes.controller';
+import { Mesas } from '../mesas/Mesas.entity';
+import { QrCodes } from './QrCodes.entity'
+import { MesasService } from 'src/mesas/mesas.service';
+
 
 @Module({
-  providers: [QrCodesService],
-  controllers: [QrcodesController]
+    imports: [TypeOrmModule.forFeature([QrCodes]),
+              TypeOrmModule.forFeature([Mesas])],
+    providers: [QrCodesService, MesasService],
+    controllers: [QrcodesController]
 })
 export class QrCodesModule {}
