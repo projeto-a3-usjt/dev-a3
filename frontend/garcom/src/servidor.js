94e14bc6ab77da079c640b39f045fd4c1ed80946
Eduardo Peres <edu@gcperes.com>
2024-06-14 20:09:38 -0300

    front/garçom: adicionar getQrs

--- frontend/garcom/src/servidor.js
+++ frontend/garcom/src/servidor.js
@@ -1,16 +1,27 @@
-const url = "http://localhost:3001/cadastros/login"
+import axios from "axios"
+
+const url = "http://localhost:3001"
 
 export async function login(username, senha) {
-		try {
-				let credenciais = { username: username, senha: senha }
-				let r = await fetch(url, {
-						method: "POST",
-						body: JSON.stringify(credenciais),
-						headers: { 'Content-Type': 'application/json' }
-				})
-				return r
-		} catch (e) {
-				// somente ocorrerá quando o servidor não responder
-				return e
-		}
+	try {
+		let credenciais = { username: username, senha: senha }
+	  let r = await fetch(`${url}/cadastros/login`, {
+      method: "POST",
+      body: JSON.stringify(credenciais),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    return r
+  } catch (e) {
+    // somente ocorrerá quando o servidor não responder
+    return e
+  }
+}
+
+export async function getQrs() {
+  try {
+    const r = await axios.get(`${url}/qrcodes`)
+    return r
+  } catch (e) {
+    return e
+  }
 }
