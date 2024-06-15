d257d8415e71d87cc3ba48becb5e02cebc0b0cf3
Eduardo Peres <edu@gcperes.com>
2024-06-15 19:42:40 -0300

    front/garçom: finalizar gerenciamento de qr codes

--- frontend/garcom/src/componentes/MesaCard.js
+++ frontend/garcom/src/componentes/MesaCard.js
@@ -1,31 +1,68 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Button, CloseButton, FormControl, InputGroup } from "react-bootstrap"
-import React from "react";
+import { delByMesa, newQr } from "../servidor";
+import { QRCode } from "react-qr-code"
+import { Button, CloseButton, FormControl, InputGroup, Modal } from "react-bootstrap"
+import React, { useState, useRef } from "react";
 
-function QrCodeEntry({ nome }) {
+function QrCodeEntry({ nome, hash }) {
+
+  const [showModal, setShowModal] = useState(false)
+
+  const handleShow = () => setShowModal(true)
+  const handleClose = () => setShowModal(false)
+  
   return (
-    <div className="m-3 container border w-25 d-flex flex-column text-center">
-      <b>{nome}</b>
-      <Button className="m-3" variant="primary">Mostrar QRCode</Button>
-    </div>
+    <>
+      <div className="m-3 container border vw-w-25 d-flex flex-column text-center">
+        <b>{nome}</b>
+        <Button className="m-3" variant="primary" onClick={handleShow}>Mostrar QRCode</Button>
+      </div>
+
+      { !!showModal &&
+        <Modal size="lg" show={showModal} onHide={handleClose} backdrop="static" centered>
+          <Modal.Header closeButton>
+            <Modal.Title>{nome}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <QRCode style={{width: "100%", height: "100%"}} value={"https://localhost:3002/?c=" + hash}/>
+          </Modal.Body>
+          <Modal.Footer>{"https://localhost:3002/?c=" + hash}</Modal.Footer>
+        </Modal>
+      }
+    </>
   )
 }
 
 export default function MesaCard({ numMesa, qrArray }) {
 
-  function forQrs(array) {
-    return array.map(qr => <QrCodeEntry nome={qr.nome} />);
+  const [qrs, setQrs] = useState(JSON.parse(qrArray))
+  
+  const inputRef = useRef(null)
+  
+  async function handleAdd() {
+    const value = inputRef.current.value
+
+    if (value.trim()) {
+      let qr = await newQr(numMesa, value)
+      setQrs([...qrs, qr])
+      inputRef.current.value = ''
+    }
+  }
+
+  async function handleWipe() {
+    await delByMesa(numMesa)
+    setQrs([])
   }
   
   return (
     <div className="card mx-auto w-100 m-4">
-      <div className="card-header d-flex justify-content-between">Mesa {numMesa} <CloseButton/></div>
+      <div className="card-header d-flex justify-content-between">Mesa {numMesa} <CloseButton onClick={handleWipe}/></div>
       <div className="d-flex overflow-auto w-100 align-items-center">
-        { forQrs(JSON.parse(qrArray)) }
+        { qrs.map(qr => <QrCodeEntry nome={qr.nome} hash={qr.hash} />) }
       </div>
       <InputGroup>
-        <FormControl placeholder="Nome do cliente"></FormControl>
-        <Button variant="success">Adicionar</Button>
+        <FormControl placeholder="Nome do cliente" ref={inputRef}></FormControl>
+        <Button variant="success" onClick={handleAdd}>Adicionar</Button>
       </InputGroup>
     </div>
   )
